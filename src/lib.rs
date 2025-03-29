0f5b861a350ab96c51572af3427dc5f07c6dc13c
Markus Mayer <widemeadows@gmail.com>
2025-03-29 10:02:29 +0100

    Force the implementatin of the system code

--- src/lib.rs
+++ src/lib.rs
@@ -202,26 +202,28 @@ fn generate_system_code(ecs: &Ecs) -> String {
         "/// Marker trait for systems.\npub trait System: 'static + Send + Sync {{\n    const ID: SystemId;\n}}\n\n",
     ));
 
-    for (id, component) in ecs.systems.iter().enumerate() {
+    for (id, system) in ecs.systems.iter().enumerate() {
         generated_code.push_str(&format!(
             "#[derive(Debug, Clone)]\npub struct {name}({name}Data);\n",
-            name = component.name.type_name,
+            name = system.name.type_name,
         ));
 
         // TODO: How can we require that the system has this trait while not implementing it ourselves?
         generated_code.push_str(&format!(
             "\npub trait Apply{name} {{\n",
-            name = component.name.type_name,
+            name = system.name.type_name,
         ));
+        generated_code.push_str("    /// Apply the system to the given entity components\n");
+        // TODO Generate documentation comments for the components
         generated_code.push_str("    fn apply(&self");
-        for input in &component.inputs {
+        for input in &system.inputs {
             generated_code.push_str(&format!(
                 ", {field_name}: &{type_name}",
                 field_name = input.field_name,
                 type_name = input.type_name
             ));
         }
-        for output in &component.outputs {
+        for output in &system.outputs {
             generated_code.push_str(&format!(
                 ", {field_name}: &mut {type_name}",
                 field_name = output.field_name,
@@ -231,24 +233,57 @@ fn generate_system_code(ecs: &Ecs) -> String {
         generated_code.push_str(");\n");
         generated_code.push_str("}\n");
 
+        // TODO: Enforce the trait implementation.
+        generated_code.push_str(&format!("\nimpl {name} {{\n", name = system.name.type_name,));
+        generated_code.push_str("    /// Apply the system to the given entity components\n");
+        generated_code.push_str("    #[inline]\n");
+        generated_code.push_str("    pub fn apply(&self");
+        for input in &system.inputs {
+            generated_code.push_str(&format!(
+                ", {field_name}: &{type_name}",
+                field_name = input.field_name,
+                type_name = input.type_name
+            ));
+        }
+        for output in &system.outputs {
+            generated_code.push_str(&format!(
+                ", {field_name}: &mut {type_name}",
+                field_name = output.field_name,
+                type_name = output.type_name
+            ));
+        }
+        generated_code.push_str(") {\n");
+        generated_code.push_str(&format!(
+            "        // Enforce the implementation of the system trait\n        Apply{name}::apply(self",
+            name = system.name.type_name
+        ));
+        for input in &system.inputs {
+            generated_code.push_str(&format!(", {field_name}", field_name = input.field_name));
+        }
+        for output in &system.outputs {
+            generated_code.push_str(&format!(", {field_name}", field_name = output.field_name));
+        }
+        generated_code.push_str(");\n    }\n");
+        generated_code.push_str("}\n");
+
         generated_code.push_str(&format!(
             "\n#[automatically_derived]\nimpl System for {name} {{\n    const ID: SystemId = SystemId({id});\n}}\n",
-            name = component.name.type_name
+            name = system.name.type_name
         ));
 
         generated_code.push_str(&format!(
             "\n#[automatically_derived]\nimpl From<{name}Data> for {name} {{\n    fn from(data: {name}Data) -> Self {{\n        Self(data)\n    }}\n}}\n",
-            name = component.name.type_name,
+            name = system.name.type_name,
         ));
 
         generated_code.push_str(&format!(
             "\n#[automatically_derived]\nimpl std::ops::Deref for {name} {{\n    type Target = {name}Data;\n\n    fn deref(&self) -> &Self::Target {{\n        &self.0\n    }}\n}}\n\n",
-            name = component.name.type_name,
+            name = system.name.type_name,
         ));
 
         generated_code.push_str(&format!(
             "#[automatically_derived]\nimpl std::ops::DerefMut for {name} {{\n    fn deref_mut(&mut self) -> &mut Self::Target {{\n        &mut self.0\n    }}\n}}\n\n",
-            name = component.name.type_name
+            name = system.name.type_name
         ));
     }
     generated_code
