9684e1530c188cfdad8627d8f2ea12cf1a89ba1e
Markus Mayer <widemeadows@gmail.com>
2025-03-30 17:34:56 +0200

    Ensure no duplicate components are used

--- src/ecs.rs
+++ src/ecs.rs
@@ -2,7 +2,7 @@ use crate::archetype::Archetype;
 use crate::component::Component;
 use crate::system::System;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Ecs {
@@ -13,10 +13,16 @@ pub struct Ecs {
 
 #[derive(thiserror::Error, Debug)]
 pub enum EcsError {
+    #[error("Component '{0}' is defined more than once.")]
+    DuplicateComponentDefinition(String),
     #[error("Component '{0}' in archetype '{1}' is not defined in the ECS components.")]
     MissingComponentInArchetype(String, String),
+    #[error("Component '{0}' in archetype '{1}' is referenced more than once.")]
+    DuplicateComponentInArchetype(String, String),
     #[error("Component '{0}' in system '{1}' is not defined in the ECS components.")]
     MissingComponentInSystem(String, String),
+    #[error("Component '{0}' in system '{1}' is referenced more than once.")]
+    DuplicateComponentInSystem(String, String),
     #[error("Duplicate archetype '{0}' and '{1}'")]
     DuplicateArchetype(String, String),
     #[error("Failed to process template: {0}")]
@@ -48,14 +54,25 @@ impl Ecs {
 
     /// Ensure that all components used by archetypes are defined in the components vector of the ECS.
     pub(crate) fn ensure_component_consistency(&self) -> Result<(), EcsError> {
-        let defined_components: std::collections::HashSet<_> = self
-            .components
-            .iter()
-            .map(|component| &component.name)
-            .collect();
+        let mut defined_components = HashSet::new();
+        for component in &self.components {
+            if !defined_components.insert(&component.name) {
+                return Err(EcsError::DuplicateComponentDefinition(
+                    component.name.type_name.clone(),
+                ));
+            }
+        }
 
         for archetype in &self.archetypes {
+            let mut archetype_components = HashSet::new();
             for component_ref in &archetype.components {
+                if !archetype_components.insert(component_ref) {
+                    return Err(EcsError::DuplicateComponentInArchetype(
+                        component_ref.type_name.clone(),
+                        archetype.name.type_name.clone(),
+                    ));
+                }
+
                 if !defined_components.contains(component_ref) {
                     return Err(EcsError::MissingComponentInArchetype(
                         component_ref.type_name.clone(),
@@ -65,9 +82,17 @@ impl Ecs {
             }
         }
 
+        let mut system_components = HashSet::new();
         for system in &self.systems {
             // Validate system inputs
             for component_ref in &system.inputs {
+                if !system_components.insert(component_ref) {
+                    return Err(EcsError::DuplicateComponentInSystem(
+                        component_ref.type_name.clone(),
+                        system.name.type_name.clone(),
+                    ));
+                }
+
                 if !defined_components.contains(component_ref) {
                     return Err(EcsError::MissingComponentInSystem(
                         component_ref.type_name.clone(),
@@ -78,6 +103,13 @@ impl Ecs {
 
             // Validate system outputs
             for component_ref in &system.outputs {
+                if !system_components.insert(component_ref) {
+                    return Err(EcsError::DuplicateComponentInSystem(
+                        component_ref.type_name.clone(),
+                        system.name.type_name.clone(),
+                    ));
+                }
+
                 if !defined_components.contains(component_ref) {
                     return Err(EcsError::MissingComponentInSystem(
                         component_ref.type_name.clone(),
