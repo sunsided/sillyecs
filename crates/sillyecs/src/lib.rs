29e69da9d960305624192f75c25daafbd3564cf7
Markus Mayer <widemeadows@gmail.com>
2025-04-17 17:50:17 +0200

    Move WorldId to support library

--- crates/sillyecs/src/lib.rs
+++ crates/sillyecs/src/lib.rs
@@ -4,7 +4,9 @@ mod archetypes;
 mod entity_id;
 mod flatten_slices;
 mod flatten_slices_mut;
+mod world_id;
 
 pub use entity_id::EntityId;
 pub use flatten_slices::FlattenSlices;
 pub use flatten_slices_mut::FlattenSlicesMut;
+pub use world_id::WorldId;
