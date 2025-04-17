befc526eeac537b23231665f8f1b4839c7c6d22f
Markus Mayer <widemeadows@gmail.com>
2025-04-17 17:44:37 +0200

    Move entity ID to support library

--- crates/sillyecs/src/lib.rs
+++ crates/sillyecs/src/lib.rs
@@ -1,6 +1,10 @@
 //! # Utility functions for `sillyecs`.
 
+mod archetypes;
+mod entity_id;
 mod flatten_slices;
 mod flatten_slices_mut;
+
+pub use entity_id::EntityId;
 pub use flatten_slices::FlattenSlices;
 pub use flatten_slices_mut::FlattenSlicesMut;
